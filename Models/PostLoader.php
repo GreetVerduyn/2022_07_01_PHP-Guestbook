76563e07214d263ae1ad84fee2dc2db4881eed83
Greet Verduyn <greetverduyn@proximus.be>
2022-07-01 16:34:33 +0200

    show post on screen

--- Models/PostLoader.php
+++ Models/PostLoader.php
@@ -5,6 +5,29 @@ declare(strict_types=1);
 class PostLoader
 {
 
+   /* public function (savePost){Post $post){
+
+      }*/
 
 
+    public function getPosts(){
+        return json_decode(file_get_contents());
+    }
 }
+
+
+//Functie van BRIAN
+//
+//public function savePost(POST $post){
+//        $data=[];
+//        $data['title']=$post->getTitle();
+//        $data['date']=$post->getDate();
+//        $data['message']=$post->getMessage();
+//        $data['author']=$post->getAuthor();;
+//        var_dump($data);
+//        $dataJSON = json_encode($data);
+//        file_put_contents('D:\WebPages\www\challenge-php-guestbook\db\db.txt',$dataJSON,FILE_APPEND);
+//    }
+//
+//2
+//[14:32]
--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php
 declare(strict_types=1);
 
+require_once ('./Models/Post.php');
+require_once ('./Models/PostLoader.php');
 
 
 
+$path= "./posts.json";
+
+$file = file_get_contents($path); // = string
+//var_dump($file);
+
+$jsonContent= json_decode($file);
+//$jsonContent2= json_decode($file, true);
+$jsonArray = $jsonContent->posts;
+//$jsonArray2 = $jsonContent2->posts; //array
+var_dump('jsonArray',$jsonArray);
+echo '<br>';
+echo '<br>';
 
 
+//var_dump('jsonArray2', $jsonArray2);
 
 ?>
 
@@ -20,13 +35,43 @@ declare(strict_types=1);
       <header>
           <?php include "header.php"; ?>
       </header>
-+
+      <hr>
+      <hr>
 
 
+      <?php
+      // Decode JSON data to PHP associative array
+      // Loop through the associative array
+
+      foreach($jsonArray as $key=>$value){
+          echo $key . "=>" . $value->title . "<br>";
+      }
+      echo "<br>";
+
+      // Decode JSON data to PHP object
+      // Loop through the object
+     /* foreach($jsonArray as $key=>$value){
+          echo $key . "=>" . $value . "<br>";
+      }
+      */?>
 
 
+      <form action="">
+          <label for="title">Title:</label>
+          <input type="text" id="title" name="title"><br><br>
+
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name"><br><br>
+
+          <label for="lname">Last name:</label>
+          <input type="text" id="lname" name="lname"><br><br>
+
+          <input type="submit" value="Submit">
+      </form>
 
 
+      <hr>
+      <hr>
 
     <footer>
     <?php include "footer.php"; ?>
