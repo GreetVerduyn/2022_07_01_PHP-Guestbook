0753d4b3780e9a5be2454f24b33fe4ec70da5c5c
Greet Verduyn <greetverduyn@proximus.be>
2022-07-01 10:39:12 +0200

    basic format

---
+++ PHP/footer.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+</head>
+<body>
+<footer>
+
+</footer>
+</body>
+
+</html>
---
+++ PHP/header.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <title>Guest Book</title>
+    <link rel="stylesheet" href="../styles.css">
+</head>
+<body>
+
+
+
+
+</body>
+
+</html>
